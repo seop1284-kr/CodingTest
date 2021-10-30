dc75f816a026f281b2eca453b3224df44590804b
Kim Jinseob <seop1284@naver.com>
2021-10-30 22:31:48 +0900

    lv2 문자열 압축(수정)
    문자열 다루기

--- 문자열 압축.py
+++ 문자열 압축.py
@@ -2,12 +2,17 @@
 def solution(s):
     answer = 0
     ans = ''
-
+    if len(s) == 1:
+        return 1
+    
+    # 문자열 자르는 단위를 1 부터 문자열 길이 까지 for 문
     for n in range(1, len(s)):
+        ans = ''
         cnt = 1
         temp = s[:n]
-        print(len(s) // n)
-        for l in range(1, len(s) // n):
+        # 마지막 검사를 위해 len(s) + 1 까지 체크한다.
+        for l in range(1, len(s) + 1):
+            
             if temp == s[n * l : n + n * l]:
                 cnt += 1
             else:
@@ -16,18 +21,10 @@ def solution(s):
                     ans += str(cnt)
                 cnt = 1
                 temp = s[n * l : n + n * l]
-            
-            # 마지막
-            if l == len(s) - 1:
-                ans += temp
-                if cnt != 1:
-                    ans += str(cnt)
-                cnt = 1
-                
-        
-        print(ans)
+
+        # 결과 길이를 재서 최소 값을 저장
         if answer == 0 or answer > len(ans):
             answer = len(ans)
-        ans = ''
+        
     
     return answer
