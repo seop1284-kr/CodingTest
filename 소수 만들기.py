7679035e0ce873041e88f6149fdb727d3bbdc9cb
Kim Jinseob <seop1284@naver.com>
2021-11-06 23:07:25 +0900

    lv1. 소수 만들기(완성, 답안 2개)
    itertools의 combinations 사용

--- 소수 만들기.py
+++ 소수 만들기.py
@@ -31,3 +31,26 @@ def check(arr, nums, r):
             return False
     
     return True
+
+# 다른답안
+# https://programmers.co.kr/learn/courses/30/lessons/12977?language=python3
+# itertools의 combinations를 사용하면 엄청 간단 시간초과도 안남
+from itertools import combinations
+
+def solution(nums):
+    answer = 0
+    for v in combinations(nums, 3):
+        if check(v):
+            answer += 1
+    return answer
+
+def check(arr):
+    sum = 0
+    for i in range(3):
+        sum += arr[i]
+    
+    for i in range(2, sum - 1):
+        if sum % i == 0:
+            return False
+    
+    return True
