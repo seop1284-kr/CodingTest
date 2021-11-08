fdb42209f11c07013f2fcd4c5dd3837b5137aebf
Kim Jinseob <seop1284@naver.com>
2021-11-09 00:21:30 +0900

    lv3. 가장 먼 노드(문제 해결)
    bfs + 인접행렬 사용하여 시간초과 해결

--- 가장 먼 노드.py
+++ 가장 먼 노드.py
@@ -24,3 +24,34 @@ def solution(n, edge):
         queue = temp
                 
     return answer
+
+# https://programmers.co.kr/learn/courses/30/lessons/49189?language=python3
+# 인접 행렬 사용하여 시간 초과 해결
+import copy
+
+def solution(n, edge):
+    
+    # 인접 행렬 만듬
+    adjs = [[] for row in range(n + 1)]
+    for v in edge:
+        adjs[v[0]].append(v[1])
+        adjs[v[1]].append(v[0])
+        
+    # 방문 노드
+    visited = [False for i in range(n + 1)]
+    visited[1] = True
+    
+    queue = [1]
+    temp = []
+
+    while queue:
+        temp = copy.deepcopy(queue)
+        for n in temp:
+            queue.pop(0)
+            for tnode in adjs[n]:
+                if visited[tnode]:
+                    continue
+                visited[tnode] = True
+                queue.append(tnode)
+    
+    return len(temp)
