e5feb63ba933deb97dc43da61a24e485ef2d7949
Kim Jinseob <seop1284@naver.com>
2021-11-22 02:55:37 +0900

    lv2. 땅따먹기(수정완료)
    재귀 대신에 중첩 for문으로 해결

--- 땅따먹기.py
+++ 땅따먹기.py
@@ -1,24 +1,9 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12913
-max_sum = 0
 
 def solution(land):
-    answer = 0    
-            
-    dfs(0, -1, 0, len(land), land)
-    print(len(land))
-    return max_sum
-
-def dfs(d, idx, s, l, land):
-    global max_sum
     
-    if d == l:
-        if max_sum < s:
-            max_sum = s
-        return
+    for n in range(1, len(land)):
+        for i in range(4):
+            land[n][i] += max(land[n - 1][:i] + land[n - 1][i + 1:])
     
-    for i in range(4):
-        
-        if i is idx:
-            continue
-        
-        dfs(d + 1, i, s + land[d][i], l, land)
+    return max(land[-1])
