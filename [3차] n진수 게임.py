c8a3fdc28b71fe3af08837af89b7123aa5596b2d
Kim Jinseob <seop1284@naver.com>
2021-11-24 17:35:46 +0900

    lv2. [3차] n진수 게임(해결)
    카카오

--- [3차] n진수 게임.py
+++ [3차] n진수 게임.py
@@ -2,24 +2,29 @@
 
 def solution(n, t, m, p):
     answer = ''
-    cnt = 1
-    number = 1
-    q = '0'
     
-    while t:
-        if len(q) < cnt:
-            q += to_n_number(number, n)
-            number += 1
-        
-        num = q[cnt - 1]
-        if cnt % m == p:
-            answer += num
-            t -= 1
-            
-        cnt += 1
-
+    # p의 순서 인덱스 배열
+    porder = [i * m + p for i in range(t)]
+    
+    # 가장 높은 p의 순서 인덱스
+    pmax = max(porder)
+    
+    # 시작 숫자
+    number = 1
+    s = '0'
+    
+    # pmax를 얻을 수 있을 때 까지 n진수 문자열 나열함 
+    while pmax >= len(s):
+        s += to_n_number(number, n)
+        number += 1
+    
+    # 결과 가져오기
+    for i in porder:
+        answer += s[i - 1]
+    
     return answer
 
+# n진수 문자열 구하기
 def to_n_number(number, n):
     nc = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
     temp = ''
