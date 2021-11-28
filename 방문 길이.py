5b1ecf4e0b43a3ee3cec8fa07e6b66c480c80d62
Kim Jinseob <seop1284@naver.com>
2021-11-28 16:20:05 +0900

    lv2. 방문 길이(해결, 알고리즘은 맞는데 결과를 잘못 계산했었음)
    노드가 아닌 선에 대한 방문 리스트를 만듬

--- 방문 길이.py
+++ 방문 길이.py
@@ -6,24 +6,27 @@ def solution(dirs):
      
     x, y = 5, 5
      
-    for i in range(len(dirs)):
-        v = dirs[i]
+    for v in dirs:
         if v == 'L' and x > 0:
             x -= 1
             xvisited[y][x] = 1
-
+            print(v, x, y)
         elif v == 'R' and x < 10:
             xvisited[y][x] = 1
             x += 1
+            print(v, x, y)
 
         elif v == 'U' and y < 10:
             yvisited[y][x] = 1
             y += 1
-
+            print(v, x, y)
         elif v == 'D' and y > 0:
             y -= 1
             yvisited[y][x] = 1
-
-    for row in zip(*xvisited, *yvisited):
-        answer += sum(row)
+            print(v, x, y)
+            
+    for r in xvisited:
+        answer += sum(r)
+    for r in yvisited:
+        answer += sum(r)
     return answer
