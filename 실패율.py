7092873f8489aa5b6d021f6fec54d3fd2d7a4c1e
Kim Jinseob <seop1284@naver.com>
2021-12-01 23:50:22 +0900

    lv1. 실패율(성공!)
    N=5, (4, 4, 4, 4, 4)와 같은 경우 0으로 나눠져 에러가 생김

--- 실패율.py
+++ 실패율.py
@@ -5,6 +5,9 @@ def solution(N, stages):
     people_num = len(stages)
     lst = []
     for i in range(1, N + 1):
+        if people_num == 0:
+            lst.append((i, 0))
+            continue
         lst.append((i, stages.count(i) / people_num))
         people_num -= stages.count(i)
     lst.sort(reverse = True, key = lambda element : element[1])
