7faca8f9183139a65c5a15a6fe302c467ff5552e
Kim Jinseob <seop1284@naver.com>
2022-01-15 00:37:55 +0900

    문제 추가

--- sql/GROUP BY.sql
+++ sql/GROUP BY.sql
@@ -1,2 +1,15 @@
 -- 고양이와 개는 몇마리 있을까? https://programmers.co.kr/learn/courses/30/lessons/59040
-SELECT ANIMAL_TYPE, COUNT(*) AS count FROM ANIMAL_INS GROUP BY ANIMAL_TYPE
+SELECT ANIMAL_TYPE, COUNT(*) AS count FROM ANIMAL_INS GROUP BY ANIMAL_TYPE ORDER BY ANIMAL_TYPE;
+
+-- 동명 동물 수 찾기 https://programmers.co.kr/learn/courses/30/lessons/59041
+SELECT NAME, COUNT(*) as COUNT FROM ANIMAL_INS WHERE NAME is not NULL GROUP BY NAME HAVING COUNT >= 2 ORDER BY NAME;
+
+-- 입양 시각 구하기(1) https://programmers.co.kr/learn/courses/30/lessons/59412
+SELECT HOUR(DATETIME) AS 'HOUR', COUNT(*) AS COUNT 
+FROM ANIMAL_OUTS 
+WHERE HOUR(DATETIME) >= 9 and HOUR(DATETIME) < 20
+GROUP BY HOUR
+ORDER BY HOUR;
+
+-- 입양 시각 구하기(2) https://programmers.co.kr/learn/courses/30/lessons/59413
+-- 다시 보기
