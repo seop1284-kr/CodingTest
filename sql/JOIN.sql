2e12f59d823d8ca078d1399c67b43b296f31874b
Kim Jinseob <seop1284@naver.com>
2022-01-22 19:21:17 +0900

    문제 추가

--- sql/JOIN.sql
+++ sql/JOIN.sql
@@ -5,6 +5,14 @@ RIGHT JOIN ANIMAL_OUTS o
 ON i.ANIMAL_ID = o.ANIMAL_ID
 WHERE i.ANIMAL_ID is NULL;
 
+-- 있었는데요 없었습니다 https://programmers.co.kr/learn/courses/30/lessons/59043
+SELECT i.ANIMAL_ID, i.NAME
+FROM ANIMAL_INS i
+LEFT JOIN ANIMAL_OUTS o
+ON i.ANIMAL_ID = o.ANIMAL_ID
+WHERE i.DATETIME > o.DATETIME
+ORDER BY i.DATETIME;
+
 -- MySQL은 FULL OUTER JOIN이 없다
 -- 다음과 같이 실행한다
 SELECT i.ANIMAL_ID, i.NAME 
