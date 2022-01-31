44596398e967bfff414a650ffe3d058303c41c71
Kim Jinseob <seop1284@naver.com>
2022-01-31 23:01:36 +0900

    문제 추가

--- sql/String, Date.sql
+++ sql/String, Date.sql
@@ -2,3 +2,9 @@
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
 WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+
+-- 이름에 el이 들어가는 동물 찾기 https://programmers.co.kr/learn/courses/30/lessons/59047
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE NAME LIKE '%EL%' AND ANIMAL_TYPE = 'Dog'
+ORDER BY NAME
